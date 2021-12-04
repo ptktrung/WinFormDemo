1c8b37d63c4e62da7e1b912b682051cb6e58e3a5
ptktrung <ptktrung@gmail.com>
2021-12-04 14:28:27 +0700

    Add files via upload

---
+++ Lab06/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06
+{
+    public class Person
+    {
+        public int PersonID { get; set; }
+        public String Name { get; set; }
+        public String Address { get; set; }
+        public String Job { get; set; }
+    }
+}
